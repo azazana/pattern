e26e8e0b2b4ffa19191d581ab93c5d2b08699191
azazana <aazazana@gmail.com>
2024-04-09 19:38:46 +0300

    chain

--- chain_responce_exisice.py
+++ chain_responce_exisice.py
@@ -1,6 +1,22 @@
 import unittest
 
 
+class Event(list):
+    def __call__(self, *args, **kwargs):
+        for item in self:
+            args[0](item)
+
+
+class Game:
+    def __init__(self):
+        self.creatures = []
+        self.queries = Event()
+
+    def check_creatures(self, sender):
+        self.creatures(sender)
+
+
+
 class Creature:
     def __init__(self, game, attack, defense):
         self.initial_defense = defense
@@ -9,26 +25,33 @@ class Creature:
 
     @property
     def attack(self):
+        self.game.check_creatures(self.handle)
         return self.initial_attack
 
     @property
     def defense(self):
+        self.game.check_creatures(self.handle)
         return self.initial_defense
+
+    def handle(self, sender, query):
+        pass
 
 
 class Goblin(Creature):
     def __init__(self, game, attack=1, defense=1):
         super().__init__(game, attack, defense)
 
+    def handle(self, sender):
+        if isinstance(sender, GoblinKing):
+            self.attack += 1
+        if isinstance(sender, Goblin):
+            print('Goblin')
+            self.defense += 1
+
 
 class GoblinKing(Goblin):
     def __init__(self, game):
         super().__init__(game, 3, 3)
-
-
-class Game:
-    def __init__(self):
-        self.creatures = []
 
 
 class FirstTestSuite(unittest.TestCase):
@@ -39,3 +62,6 @@ class FirstTestSuite(unittest.TestCase):
 
         self.assertEqual(1, goblin.attack)
         self.assertEqual(1, goblin.defense)
+        goblin2 = Goblin(game)
+        game.creatures.append(goblin2)
+        self.assertEqual(2, goblin2.defense)
